50a3eb7e0a2538e107310040e6794a4988a6c871
FWJK35 <calvinrodrigue@gmail.com>
2022-11-03 14:00:33 -0700

    added some keywords

--- Keywords.java
+++ Keywords.java
@@ -3,8 +3,9 @@ public class Keywords {
     public static final String IF_KEYWORD = "lmao";
     public static final String DEFINE_VARIABLE_KEYWORD = "aka";
     public static final String DEFINE_FUNCTION_KEYWORD = "wdym";
-    public static final String DECREMENT_KEYWORD = "dumb";
-    public static final String INCREMENT_KEYWORD = "fat";
+    public static final String DECREMENT_KEYWORD = "kys";
+    public static final String INCREMENT_KEYWORD = "ily";
     public static final String PRINT_KEYWORD = "xd";
+    public static final String INPUT_KEYWORD = "hmu";
     public static final String RETURN_KEYWORD = "ttyl";
 }
