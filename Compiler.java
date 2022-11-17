a114d979b004ae91983fb7f60cc8b7579e8bee6f
FWJK35 <calvinrodrigue@gmail.com>
2022-11-17 13:57:59 -0800

    Added JOptionPane and code to read print statment
    Co-authored-by: apidges <apidges@users.noreply.github.com>
    Co-authored-by: Jonathan510T <Jonathan510T@users.noreply.github.com>

--- Compiler.java
+++ Compiler.java
@@ -1,5 +1,10 @@
 import java.io.*;
+import java.security.Key;
 import java.util.*;
+import java.util.List;
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * Compiler
  */
@@ -7,13 +12,58 @@ public class Compiler {
 
     public static void main(String[] args) {
         test();
-
+        
+        // file input prompt using JOptionPane 
+        Scanner input = new Scanner(System.in);
         try {
-            Scanner file = new Scanner(new File("code.hrm"));
-            file.close();
-        } catch (Exception e) {}
+            String file = JOptionPane.showInputDialog("Input File Name: ");
+            input = new Scanner(new File(file));
+            runCode(input);
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.getStackTrace();
+            System.out.println("Error: Invalid File Type");
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        
+        runCode(input);
     }
 
+    public static void runCode(Scanner file) {
+        // initialize variables and stuff here
+        while (file.hasNextLine()) {
+            String thisLine = file.nextLine();
+            
+            // get all tokens on the current line
+            Scanner lineScanner = new Scanner(thisLine);
+            List<String> tokens = new ArrayList<String>();
+            while (lineScanner.hasNext()) {
+                tokens.add(lineScanner.next());
+            }
+            lineScanner.close();
+
+            // check if line is PRINT statment
+            if (tokens.size() >= TokenIndex.PRINT_TOKEN + 1) {
+                if (tokens.get(TokenIndex.PRINT_TOKEN).equals(Keywords.PRINT_KEYWORD)) {
+                    // interpret everything else after
+                    // get beginning of string literal and trim line
+                    int beginIndex = thisLine.indexOf(Keywords.STRING_LITERAL_KEYWORD);
+                    int endIndex = thisLine.indexOf(Keywords.STRING_LITERAL_KEYWORD, beginIndex + 1);
+                    String remaining = thisLine.substring(endIndex + Keywords.STRING_LITERAL_KEYWORD.length());
+                    if (remaining.length() > 0) {
+                        throw new Error("Invalid Arguments to command: " + Keywords.PRINT_KEYWORD);
+                    }
+                    else {
+                        System.out.println(thisLine.substring(beginIndex, endIndex));
+                    }
+                }
+            }
+
+        }
+    }
+    
+    
     // testing things here so it doesn't clog up the main class
     public static void test() {
         Variable intTest = new Variable(4);
