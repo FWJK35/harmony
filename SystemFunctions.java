975b0dc3d440d26f215f048013ed6a6627df7413
FWJK35 <calvinrodrigue@gmail.com>
2023-01-16 23:27:13 -0800

    idk probably unimportant but shouldnt delete

---
+++ SystemFunctions.java
@@ -0,0 +1,11 @@
+
+public class SystemFunctions {
+    public static String[] systemFunctionHeaders = {
+        "wdym int caststr int tocast :)",
+
+    };
+
+    public static Variable getSystemFunction() {
+        
+    }
+}
